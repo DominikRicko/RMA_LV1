f4b39d7e286f6ed8906d92e069fd7c14418ec093
Dominik Ričko <dominik.ricko@gmail.com>
2021-03-27 18:15:51 +0100

    Added Straight scoring /w tests

---
+++ src/main/kotlin/scores/ScoreRuleStraight.kt
@@ -0,0 +1,39 @@
+package scores
+
+import dice.Dice
+
+object ScoreRuleStraight : ScoreRule {
+
+    override fun getDiceScore(dices: Collection<Dice>): Int {
+
+        val countedSides = dices
+            .groupBy{it.side}.toSortedMap()
+            .map{ it -> it.key }.reversed()
+            .toTypedArray()
+
+        var largeStraightChecker = 6
+        var smallStraightChecker = 5
+
+        countedSides.forEach {
+            if(it == largeStraightChecker){
+                largeStraightChecker--
+            }
+
+            if(it == smallStraightChecker){
+                smallStraightChecker--
+            }
+
+            if(largeStraightChecker == 1){
+                return 40
+            }
+
+            if(smallStraightChecker == 0){
+                return 30
+            }
+        }
+
+        return 0
+
+    }
+
+}
---
+++ src/test/kotlin/scores/ScoreRuleStraightTest.kt
@@ -0,0 +1,88 @@
+package scores
+
+import dice.Dice
+import dice.SixSidedDice
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class ScoreRuleStraightTest{
+
+    lateinit var dices : ArrayList<Dice>
+
+    @BeforeEach
+    fun createDicesBeforeTest(){
+        this.dices = arrayListOf(
+            SixSidedDice(),
+            SixSidedDice(),
+            SixSidedDice(),
+            SixSidedDice(),
+            SixSidedDice(),
+            SixSidedDice()
+        )
+    }
+
+    @Test
+    fun testLargeStraight(){
+
+        this.dices[0].side = 2
+        this.dices[2].side = 3
+        this.dices[1].side = 5
+        this.dices[3].side = 4
+        this.dices[4].side = 6
+        this.dices[5].side = 6
+
+        val score = ScoreRuleStraight.getDiceScore(this.dices)
+
+        assertEquals(40, score)
+
+    }
+
+    @Test
+    fun testSmallStraight(){
+
+        this.dices[0].side = 2
+        this.dices[2].side = 3
+        this.dices[1].side = 5
+        this.dices[3].side = 4
+        this.dices[4].side = 1
+        this.dices[5].side = 1
+
+        val score = ScoreRuleStraight.getDiceScore(this.dices)
+
+        assertEquals(30, score)
+
+    }
+
+    @Test
+    fun testLargeStraightOverSmallStraight(){
+
+        this.dices[0].side = 2
+        this.dices[2].side = 3
+        this.dices[1].side = 5
+        this.dices[3].side = 4
+        this.dices[4].side = 6
+        this.dices[5].side = 1
+
+        val score = ScoreRuleStraight.getDiceScore(this.dices)
+
+        assertEquals(40, score)
+
+    }
+
+    @Test
+    fun testNoStraight(){
+
+        this.dices[0].side = 2
+        this.dices[2].side = 3
+        this.dices[1].side = 5
+        this.dices[3].side = 6
+        this.dices[4].side = 6
+        this.dices[5].side = 6
+
+        val score = ScoreRuleStraight.getDiceScore(this.dices)
+
+        assertEquals(0, score)
+
+    }
+}
