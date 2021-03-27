9c92f1ad96536c7da3d3a1fd890a294326600339
Dominik Ričko <dominik.ricko@gmail.com>
2021-03-27 16:31:38 +0100

    Added test module for ScoreRuleFull

---
+++ src/test/kotlin/scores/ScoreRuleFullTest.kt
@@ -0,0 +1,122 @@
+package scores
+
+import dice.Dice
+import dice.SixSidedDice
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class ScoreRuleFullTest{
+
+    lateinit var dices : ArrayList<Dice>
+
+    @BeforeEach
+    fun diceCollectionCreation(){
+        this.dices = arrayListOf(
+            SixSidedDice(), SixSidedDice(), SixSidedDice(),
+            SixSidedDice(), SixSidedDice(), SixSidedDice()
+        )
+    }
+
+    @Test
+    fun testGetScoreFromDicesWithSameSide(){
+
+        this.dices.forEach{it.side = 1}
+
+        val score = ScoreRuleFull.getDiceScore(this.dices)
+
+        assertEquals(0, score)
+
+    }
+
+    @Test
+    fun testGetScoreFromDicesWithTwoFourCombination(){
+
+        for( i in 0..1)
+            this.dices[i].side = 2
+
+        for( i in 2..5)
+            this.dices[i].side = 3
+
+        val score = ScoreRuleFull.getDiceScore(this.dices)
+
+        assertEquals(13, score)
+
+    }
+
+    @Test
+    fun testGetScoreFromDicesWithFourTowCombination(){
+
+        for( i in 0..3)
+            this.dices[i].side = 3
+
+        for( i in 4..5)
+            this.dices[i].side = 2
+
+        val score = ScoreRuleFull.getDiceScore(this.dices)
+
+        assertEquals(13, score)
+
+    }
+
+    @Test
+    fun testGetScoreFromDicesWithFiveOneCombination(){
+
+        this.dices[5].side = 1
+
+        for( i in 0..4)
+            this.dices[i].side = 5
+
+        val score = ScoreRuleFull.getDiceScore(this.dices)
+
+        assertEquals(0, score)
+
+    }
+
+    @Test
+    fun testGetScoreFromDicesWithThreeTwoOneCombination(){
+
+        for( i in 0..2)
+            this.dices[i].side = 3
+
+        for( i in 3..4)
+            this.dices[i].side = 2
+
+        this.dices[5].side = 1
+
+        val score = ScoreRuleFull.getDiceScore(this.dices)
+
+        assertEquals(13, score)
+
+    }
+
+    @Test
+    fun testGetScoreFromDicesWithThreeThreeCombination(){
+
+        for(i in 0..2)
+            this.dices[i].side = 3
+
+        for(i in 3..5)
+            this.dices[i].side = 4
+
+        val score = ScoreRuleFull.getDiceScore(this.dices)
+
+        assertEquals(18, score)
+
+    }
+
+    @Test
+    fun testGetScoreFromDicesWithThreeThreeReversedCombination(){
+
+        for(i in 0..2)
+            this.dices[i].side = 4
+
+        for(i in 3..5)
+            this.dices[i].side = 3
+
+        val score = ScoreRuleFull.getDiceScore(this.dices)
+
+        assertEquals(18, score)
+
+    }
+}
