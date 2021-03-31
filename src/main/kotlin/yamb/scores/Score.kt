d325af5d6cc64843c766b47c13cedfb9c5165a2f
Dominik Ričko <dominik.ricko@gmail.com>
2021-03-31 17:13:08 +0200

    Added Score class

---
+++ src/main/kotlin/yamb/scores/Score.kt
@@ -0,0 +1,15 @@
+package yamb.scores
+
+data class Score(var value: Int){
+
+    companion object{
+        const val NO_SCORE_VALUE = -1
+    }
+
+    override fun toString(): String {
+        return if(value == NO_SCORE_VALUE)
+            "-"
+        else
+            super.toString()
+    }
+}
