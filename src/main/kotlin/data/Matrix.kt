05ead5c100eed1779aac4e0d412744e1bdfabeed
Dominik Ričko <dominik.ricko@gmail.com>
2021-03-31 17:12:52 +0200

    Added Matrix class for 2D Arrays

---
+++ src/main/kotlin/data/Matrix.kt
@@ -0,0 +1,27 @@
+package data
+
+class Matrix<T>(val sizeX: Int, val sizeY: Int, private val array: Array<T>) {
+
+    companion object {
+
+        inline operator fun <reified T> invoke() = Matrix(0, 0, emptyArray<T>())
+
+        inline operator fun <reified T> invoke(xWidth: Int, yWidth: Int) =
+            Matrix(xWidth, yWidth, arrayOfNulls<T>(xWidth*yWidth))
+
+        inline operator fun <reified T> invoke(xWidth: Int, yWidth: Int, initValue: () -> (T)): Matrix<T> {
+            val array = Array(xWidth*yWidth) {
+                return@Array initValue()
+            }
+            return Matrix(xWidth, yWidth, array)
+        }
+    }
+
+    operator fun get(x: Int, y: Int) : T {
+        return array[sizeX * y + x]
+    }
+
+    operator fun set(x : Int, y: Int, value : T){
+        array[sizeX * y + x] = value
+    }
+}
