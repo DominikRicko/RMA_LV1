bf6c3519c62fbe49a92a0d12de833fe48f52e051
Dominik Ričko <dominik.ricko@gmail.com>
2021-03-31 17:48:03 +0200

    added getRow/Column methods for Matrix

--- src/main/kotlin/data/Matrix.kt
+++ src/main/kotlin/data/Matrix.kt
@@ -24,4 +24,25 @@ class Matrix<T>(val sizeX: Int, val sizeY: Int, private val array: Array<T>) {
     operator fun set(x : Int, y: Int, value : T){
         array[sizeX * y + x] = value
     }
+
+    fun getRow(y : Int) : Collection<T>{
+
+        val arrayList : ArrayList<T> = ArrayList()
+
+        for( i in y*sizeX..(y+1)*sizeX)
+            arrayList.add(array[i])
+
+        return arrayList
+    }
+
+    fun getColumn(x : Int) : Collection<T>{
+
+        val arrayList : ArrayList<T> = ArrayList()
+
+        for( i in x..sizeY*sizeX step sizeX)
+            arrayList.add(array[i])
+
+        return arrayList
+    }
+
 }
