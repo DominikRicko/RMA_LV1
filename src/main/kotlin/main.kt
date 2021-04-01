ff5169404dcde3215cba6bb347cd8cc170d8fa41
Dominik Ričko <dominik.ricko@gmail.com>
2021-04-01 23:49:27 +0200

    main function now starts Yamb game

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,10 @@
+import yamb.Yamb
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val game = Yamb(4)
+
+    game.addPlayer("Dominik", Game.PlayerType.USER)
+    game.addPlayer("Dominik", Game.PlayerType.RANDOM_AI)
+
+    game.start()
 }
